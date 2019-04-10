0915183e6fa203c752fba7bd7ce6f007add21ca1
Rabbi <rabbialrabbi@gmail.com>
2019-04-10 13:25:44 +0600

    Add Extra title

--- app/title.php
+++ app/title.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class title extends readeOnly
 {
-    public $title=['Mr','Ms','Mrs','Dr','Mx','Professor'];
+    public $title=['Mr','Ms','Mrs','Dr','Mx','Professor','Test'];
 //extends readeOnly
 }
